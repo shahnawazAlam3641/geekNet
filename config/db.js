a6de0e2ae7b7e62e209ccf23908361c677f441f5
Shahnawaz Alam <shahnawazalam3641@gmail.com>
2024-10-05 22:12:36 +0530

    Bug fix

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://shahnawazalam3641:nastoy3641@cluster0.k7zel.mongodb.net/GeekNet"
+    "mongodb+srv://shahnawazalam3641:password@cluster0.k7zel.mongodb.net/GeekNet"
   );
 };
 
